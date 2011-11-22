1e82132f38f9d4cdd74652a69e41543013345717
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-11-22 15:25:47 -0800

    client search is working, but item search isnt quite working yet

--- newcheckoutmiddle.php
+++ newcheckoutmiddle.php
@@ -36,6 +36,31 @@ function searchClient(  ) {
 	xmlhttp.send();
 	
 }
+
+function searchItem(  ) {
+	var searchStr = document.getElementById("itemsearch_txt").value;
+	// var searchStr = "test";
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.onreadystatechange=function()
+	{
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	  {
+	    document.getElementById("itemsearch_sel").innerHTML=xmlhttp.responseText;
+	  }
+	}
+	xmlhttp.open("GET","search.php?itemsearch_sel="+searchStr,true);
+	xmlhttp.send();
+	
+}
 </script>
 
 <div id="middle">
@@ -85,10 +110,10 @@ function searchClient(  ) {
 			<table class="silent">
 				<tr>
 					<td width="300">
-					<input type="text" name="searchclient" />
+					<input id="itemsearch_txt" type="text" name="searchclient" />
 					</td>
 					<td width="95">
-					<input type="button" value="Search"/>
+					<input id="itemsearch_btn" type="button" value="Search" onclick="searchItem()" />
 					</td>
 				</tr>
 				<tr>
--- search.php
+++ search.php
@@ -15,29 +15,39 @@ if ( $csel_str ) {
 
 	$client_res = mysql_query( "SELECT * FROM client 
 																WHERE first_name LIKE '%" . $csel_esc . "%'" .
-																"OR last_name LIKE '%" . $csel_esc . "%'" .
-																"OR psu_id LIKE '%" . $csel_esc . "%'" .
-																"OR phone LIKE '%" . $csel_esc . "%'" .
-																"OR email LIKE '%" . $csel_esc . "%'" .
-																"OR notes LIKE '%" . $csel_esc . "%'" .
-																"ORDER BY last_name, first_name;" );
+																" OR last_name LIKE '%" . $csel_esc . "%'" .
+																" OR psu_id LIKE '%" . $csel_esc . "%'" .
+																" OR phone LIKE '%" . $csel_esc . "%'" .
+																" OR email LIKE '%" . $csel_esc . "%'" .
+																" OR notes LIKE '%" . $csel_esc . "%'" .
+																" ORDER BY last_name, first_name;" );
 	
 	while( $row = mysql_fetch_array($client_res) ) {
 		$full_name = $row['first_name'] . ' ' . $row['last_name'];
 		printf( $sel_formatstr, $row['client_id'], $full_name );
 	}
 	
-} else if ( $isel_str ) {
+} 
+
+if ($csel_str == '' ) {
+	
+	$cr = mysql_query( "SELECT * FROM client ORDER BY last_name, first_name;" );
+	
+	while( $row = mysql_fetch_array($cr) ) {
+								$full_name = $row['first_name'] . ' ' . $row['last_name'];
+								printf( $formatstr, $row['client_id'], $full_name );
+							}
+}
+
+
+if ( $isel_str ) {
 	$isel_esc = mysql_real_escape_string( $isel_str );
 
-	$item_res = mysql_query( "SELECT * FROM client 
-																WHERE first_name LIKE '%" . $isel_esc . "%'" .
-																"OR last_name LIKE '%" . $isel_esc . "%'" .
-																"OR psu_id LIKE '%" . $isel_esc . "%'" .
-																"OR phone LIKE '%" . $isel_esc . "%'" .
-																"OR email LIKE '%" . $isel_esc . "%'" .
-																"OR notes LIKE '%" . $isel_esc . "%'" .
-																"ORDER BY last_name, first_name;" );
+	$item_res = mysql_query( "SELECT * FROM item 
+																WHERE name LIKE '%" . $isel_esc . "%'" .
+																" OR type LIKE '%" . $isel_esc . "%'" .
+																" OR description LIKE '%" . $isel_esc . "%'" .
+																" ORDER BY name, type;" );
 	
 	while( $row = mysql_fetch_array($item_res) ) {
 		$item_name = $row['name'];
@@ -48,6 +58,20 @@ if ( $csel_str ) {
 		printf( $sel_formatstr, $row['client_id'], $item_name );
 	}
 	
+} 
+
+if ($isel_str==''){
+	
+	$ir = mysql_query( "SELECT * FROM item ORDER BY name, type;" );
+	
+	while( $row = mysql_fetch_array($ir) ) {
+								$item_name = $row['name'];
+								$item_type = $row['type'];
+								if ( $item_type ) {
+									$item_name = $item_name . '(' . $item_type . ')';
+								}
+								printf( $formatstr, $row['client_id'], $item_name );
+							}
 }
 
 
