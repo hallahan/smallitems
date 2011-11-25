b08075c15e9b87f17c67f735026ddf49f9bed494
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-11-25 15:58:04 -0800

    fixing add item and screwy field problem in css

--- additem.php
+++ additem.php
@@ -14,66 +14,21 @@
 
 <div id="middle">
 	<fieldset>
-		<legend>Add Client</legend>
+		<legend>Add Item</legend>
 	
 		<form action = "additem_db.php" method = "post">
-			<table class="silent">
-				<tr>
-					<td width="115">
-						<label for="name">Name:</label> 
-					</td>
-					<td width="180">
-						<input type="text" name="name" /><br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115">	
-						<label for="type">Type (optional):</label>
-					</td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 1"/>TYPE 1<br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115"></td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 2"/>TYPE 2<br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115"></td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 3"/>TYPE 3<br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115"></td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 4"/>TYPE 4<br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115"></td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 5"/>TYPE 5<br />
-					</td>
-				</tr>
-				<tr>
-					<td width="115"></td>
-					<td width="180">
-						<input type="radio" name="type" value="TYPE 6"/>TYPE 6<br />
-					</td>
-				</tr>
-			</table>				
-					
-			<table class="silent" align="right">
-				<tr>
-					<td width="80">
-						<input type="submit" value="Add" />
-					</td>
-				</tr>
-			</table>
-			
+			<label for="name">Name:</label> 
+			<input type="text" name="name" /><br />
+			<label for="type">Type (optional):</label>
+			<input type="radio" name="type" value="TYPE 1"/>TYPE 1<br />
+			<input type="radio" name="type" value="TYPE 2"/>TYPE 2<br />
+			<input type="radio" name="type" value="TYPE 3"/>TYPE 3<br />
+			<input type="radio" name="type" value="TYPE 4"/>TYPE 4<br />
+			<input type="radio" name="type" value="TYPE 5"/>TYPE 5<br />
+			<input type="radio" name="type" value="TYPE 6"/>TYPE 6<br />
+			<label for="name">Description:</label> 
+			<textarea id="descr" rows="4" cols="45" ></textarea>
+			<input type="submit" value="Add" />
 		</form>
 </div>
 </body>
