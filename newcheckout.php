3888d14da66d885c5e23c6db20ccca64bcc85ec9
Nicholas J Hallahan <hallahan@ruby.cat.pdx.edu>
2011-12-01 01:11:13 -0800

    new checkout did not work with new font, now does

--- newcheckout.php
+++ newcheckout.php
@@ -1,27 +1,7 @@
-<?php include 'newcheckout_cont.php'; ?>
-
-<html>
-<head>
-<meta content="en-us" http-equiv="Content-Language" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-
-<link href="style.css" rel="stylesheet" type="text/css" />
-
-<script src="./datepicker/datepicker.js"></script>
-<link href="./datepicker/datepicker.css" rel="stylesheet" type="text/css" />
-
-<style type="text/css"></style>
-<title>Small Items Checkout</title>
-</head>
-
-<body>
-
 <?php 
-	include 'menu.php'; 
+	include 'head.php'; 
 	include 'newcheckoutmiddle.php';
 	include 'staging/staging.php';
-	//include 'test/console.php';
+	include 'tail.php'             
 ?>
 
-</body>
-</html>
